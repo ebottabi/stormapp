ec4bfad6fb25cf15df6ccbbee5be97d183143139
Ebot Tabi <ebot.tabi@gmail.com>
2013-10-18 09:29:37 +0100

    layout

---
+++ run.sh
@@ -0,0 +1,1 @@
+mvn compile exec:java -Dexec.classpathScope=compile -Dexec.mainClass=com.ebottabi.Topology
---
+++ src/main/java/com/ebottabi/Topology.java
@@ -0,0 +1,9 @@
+package com.ebottabi;
+
+public class Topology 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
